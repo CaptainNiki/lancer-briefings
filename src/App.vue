298d7132d10a5a29e3b05eb234f817c0a471e8c5
CaptainN <32107345+CaptainNiki@users.noreply.github.com>
2024-11-17 17:30:07 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,7 @@ export default {
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///LCI-DEPLOYMENT///377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
           "frame": "Sagarmatha",
-          "mech": "Also Raiden"
+          "mech": "STAR BREAKER"
         },
         {
           "callsign": "Pin",
