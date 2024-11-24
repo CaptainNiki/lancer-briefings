bd867beaabed31e3c5a7f4f90d9fb1e97dd9fcf6
CaptainN <32107345+CaptainNiki@users.noreply.github.com>
2024-11-25 01:56:29 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -88,8 +88,8 @@ export default {
   data() {
     return {
       "mission_slug": "001",
-      "current_md": "001",
-      "events": "",
+      "current_md": "001_res",
+      "events": "001_res",
       "missions": [
         {
           "slug": "001",
