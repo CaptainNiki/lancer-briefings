f8ad4adbd55b6bf2ae4f6be50ea6781e914a9146
CaptainN <32107345+CaptainNiki@users.noreply.github.com>
2024-12-31 22:25:57 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -93,7 +93,7 @@ export default {
       "missions": [
         {
           "slug": "004",
-          "name": <span style="white-space: pre;">Crucible Exclusion <br/> Zone Reconaissance<span/>,
+          "name": "CEZ Reconnaissance",
           "status": "available"
         },
         {
